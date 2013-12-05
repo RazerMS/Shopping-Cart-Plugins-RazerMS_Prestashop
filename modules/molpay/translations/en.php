7387470676a8659ac684ea4c213156f6c1af0cad
eddy03 <eddytech03@gmail.com>
2013-12-05 17:20:31 +0800

    Complete module for Prestashop version 1.5.6.0
    For Prestashop version 1.5.6.0 and above

--- modules/molpay/translations/en.php
+++ modules/molpay/translations/en.php
@@ -1,4 +1,3 @@
 <?php
 global $_MODULE;
 $_MODULE = array();
-?>
---
+++ modules/molpay/views/templates/custom.php
@@ -0,0 +1,52 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" />
+        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootswatch/3.0.2/cosmo/bootstrap.min.css" />
+        <style>
+            #logo-molpay, .centerme {
+                text-align: center;
+            }
+            #logo-molpay img {
+                display: block;
+                margin-left: auto;
+                margin-right: auto;
+            }
+            h2 {
+                margin-top: 10px;
+                margin-bottom: 30px;
+            }
+            .btn-lg {
+                border-radius: 5px;
+            }
+        </style>
+    </head>
+    
+    <body>
+        <div class="container">
+            <div id="logo-molpay">
+                <img src="http://molpay.com/v2/images/logo/molpay_logo_400x160_transparent_24bit.png" class="img-responsive img-rounded" alt="MOLPay logo" />
+                <?php if($status != '00'): ?>
+                <h2 class="text-danger"><i class="fa fa-times-circle"></i> Payment Failed</h2>
+                <?php else: ?>
+                <h2 class="text-success"><i class="fa fa-check-circle"></i> Payment Completed</h2>
+                <?php endif; ?>
+                <hr>
+            </div>
+            <form action="<?= $_SERVER['REQUEST_URI'] ?>&gotoorder=" method="POST" id="molpay-form">
+                <?php foreach($_POST as $name => $value): ?>
+                <input type="hidden" name="<?= $name; ?>" value="<?= $value; ?>" />            
+                <?php endforeach; ?>
+                <div class="centerme">
+                    <button type="submit" class="btn btn-info btn-lg">Click here to continue</button>
+                </div>                
+            </form>
+            <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+            <script>
+                $(document).ready(function() {
+                    $('#molpay-form').submit();
+                });
+            </script>
+        </div>        
+    </body>
+</html>
